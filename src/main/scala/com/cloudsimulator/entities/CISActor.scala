61907f809350884baa1dc58ab767e3eb832aa77c
Amrish Jhaveri <ajhave5@uic.edu>
2019-02-23 13:51:31 -0600

    CIS changes for registering dc

--- src/main/scala/com/cloudsimulator/entities/CISActor.scala
+++ src/main/scala/com/cloudsimulator/entities/CISActor.scala
@@ -3,10 +3,26 @@ package com.cloudsimulator.entities
 import akka.actor.{Actor, ActorLogging}
 import com.cloudsimulator.entities.datacenter.DataCenterActor
 
-class CISActor(id : Long, dcList : List[DataCenterActor]) extends Actor with ActorLogging {
+import scala.collection.mutable.ListBuffer
+
+class CISActor(id: Long) extends Actor with ActorLogging {
+
+  var dcList: ListBuffer[Long] = ListBuffer()
+
+  final case class DcRegistration(id: Long)
 
   override def receive: Receive = {
-    case _ => println(s"CIS Actor created $id")
+
+    case DcRegistration(id: Long) => {
+      log.info("CIS::DcRegistration")
+      registerDc(id)
+    }
+
+    case _ => log.info(s"CIS Actor created $id")
+  }
+
+  def registerDc(id: Long): ListBuffer[Long] = {
+    dcList += id
   }
 
 }
